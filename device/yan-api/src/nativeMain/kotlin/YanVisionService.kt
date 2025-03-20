f02ba1ee5158a1bc7280cd69c4907fb64bbf130a
vickyleu <yu6564172@gmail.com>
2025-03-20 18:58:05 +0800

    initial commit

--- device/yan-api/src/nativeMain/kotlin/YanVisionService.kt
+++ device/yan-api/src/nativeMain/kotlin/YanVisionService.kt
@@ -1,8 +1,14 @@
 package com.airobot.device.yanapi
 
-import com.airobot.pythoninterop.*
+import com.airobot.pythoninterop.PyLong_AsLong
+import com.airobot.pythoninterop.PyUnicode_AsUTF8
+import com.airobot.pythoninterop.PyUnicode_FromString
+import com.airobot.pythoninterop.get_visual_task_result
+import com.airobot.pythoninterop.sync_do_face_recognition_value
+import com.airobot.pythoninterop.sync_do_gesture_recognition
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.*
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toKString
 
 /**
  * YAN设备视觉服务
@@ -12,40 +18,27 @@ import kotlinx.cinterop.*
 @OptIn(ExperimentalForeignApi::class)
 class YanVisionService {
     /**
-     * 执行人脸识别
-     *
-     * @param mode 识别模式，如"single"、"continuous"等
-     * @return 人脸识别结果，包含识别状态和内容
-     */
-    fun syncDoFaceRecognition(mode: String): Map<String, Any>? {
-        try {
-            memScoped {
-                val pyMode = PyUnicodeObject(mode.cstr.ptr.rawValue)
-                val result = sync_do_face_recognition(pyMode.reinterpret<PyObject>().ptr,0)
-                if (result != null) {
-                    return PyObjectToMap(result)
-                }
-                return null
-            }
-        } catch (e: Exception) {
-            return null
-        }
-    }
-
-    /**
      * 执行人脸识别并直接返回识别结果
      *
-     * @param mode 识别模式，如"single"、"continuous"等
      * @return 识别到的人脸信息，失败返回null
      */
-    fun syncDoFaceRecognitionValue(mode: String): String? {
+    fun syncDoFaceRecognitionValue(type: VisionFaceRecognitionType): String? {
         try {
             memScoped {
-                val pyMode = PyUnicodeObject(mode.cstr.ptr.rawValue)
-                val result = sync_do_face_recognition_value(pyMode.reinterpret<PyObject>().ptr,0)
+                val pyMode = PyUnicode_FromString(type.type)
+                val result = sync_do_face_recognition_value(pyMode, 0)
                 if (result != null) {
-                    val pyStr = PyUnicode_AsUTF8(result)
-                    return pyStr?.toKString()
+                    when (type) {
+                        VisionFaceRecognitionType.QUANTITY, VisionFaceRecognitionType.AGE -> {
+                            val pyInt = PyLong_AsLong(result)
+                            return pyInt.toString()
+                        }
+
+                        else -> {
+                            val pyStr = PyUnicode_AsUTF8(result)
+                            return pyStr?.toKString()
+                        }
+                    }
                 }
                 return null
             }
@@ -57,13 +50,11 @@ class YanVisionService {
     /**
      * 执行手势识别
      *
-     * @param mode 识别模式，如"single"、"continuous"等
      * @return 手势识别结果，包含识别状态和内容
      */
-    fun syncDoGestureRecognition(mode: String): Map<String, Any>? {
+    fun syncDoGestureRecognition(): Map<String, Any>? {
         try {
             memScoped {
-//                val pyMode = PyUnicodeObject(mode.cstr.ptr.rawValue)
                 val result = sync_do_gesture_recognition(0)
                 if (result != null) {
                     return PyObjectToMap(result)
@@ -82,16 +73,12 @@ class YanVisionService {
      * @param taskId 任务ID
      * @return 任务结果，包含任务状态和内容
      */
-    fun getVisualTaskResult(taskType: String, taskId: String): Map<String, Any>? {
+    fun getVisualTaskResult(taskType: VisionOption): Map<String, Any>? {
         try {
             memScoped {
-                val pyTaskType = PyUnicodeObject(taskType.cstr.ptr.rawValue)
-                val pyTaskId = PyUnicodeObject(taskId.cstr.ptr.rawValue)
-                val result = get_visual_task_result(
-                    pyTaskType.reinterpret<PyObject>().ptr,
-                    pyTaskId.reinterpret<PyObject>().ptr,
-                    0
-                )
+                val pyTaskOption = PyUnicode_FromString(taskType.value)
+                val pyTaskType = PyUnicode_FromString(taskType.childOption)
+                val result = get_visual_task_result(pyTaskOption, pyTaskType, 0)
                 if (result != null) {
                     return PyObjectToMap(result)
                 }
@@ -101,4 +88,41 @@ class YanVisionService {
             return null
         }
     }
+}
+
+sealed class VisionOption(val value: String, open val childOption: String) {
+    sealed class FaceOption(override val childOption: String) : VisionOption("face", childOption) {
+        object AGE : FaceOption("age")
+        object GENDER : FaceOption("gender")
+        object AGE_GROUP : FaceOption("age_group")
+        object QUANTITY : FaceOption("quantity")
+        object EXPRESSION : FaceOption("expression")
+        object RECOGNITION : FaceOption("recognition")
+        object TRACKING : FaceOption("tracking")
+        object MASK : FaceOption("mask")
+        object GLASS : FaceOption("glass")
+    }
+
+    sealed class OBJECT(override val childOption: String) : VisionOption("object", childOption) {
+        object RECOGNITION : OBJECT("recognition")
+    }
+
+    sealed class COLOR(override val childOption: String) : VisionOption("color", childOption) {
+        object COLOR_DETECT : COLOR("color_detect")
+    }
+
+    sealed class HAND(override val childOption: String) : VisionOption("hand", childOption) {
+        object GESTURE : HAND("gesture")
+    }
+}
+
+enum class VisionFaceRecognitionType(val type: String) {
+    RECOGNITION("recognition"),
+    QUANTITY("quantity"),
+    AGE_GROUP("age_group"),
+    GENDER("gender"),
+    AGE("age"),
+    EXPRESSION("expression"),
+    MASK("mask"),
+    GLASS("glass");
 }
